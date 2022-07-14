cc6cde9414a5ab7dc3786892e598cf1969c3bcc9
S.D <szopakleks@gmail.com>
2022-07-14 14:23:17 +0200

    works fine need to be fetch

--- WebApi/wwwroot/js/Main.js
+++ WebApi/wwwroot/js/Main.js
@@ -1,116 +1,67 @@
-﻿
-
-window.addEventListener('load', () => {
-    const form = document.querySelector("#new-task-form");
-   const input = document.querySelector("#new-task-input");
-   const list_el = document.querySelector("#tasks");
-
-
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        const task = input.value; // zmienna przypisana do task pobrana z pola
-        if (!task) {
-            alert("Wpisz treść przed dodaniem");
-            return;
-        }  
-
-        const task_el = document.createElement("div"); //  tworzy kontener
-        task_el.classList.add("task");  // przypisuje element do contenera
-         // a wiec mozna do niego juz cos pobrac z api?
-
-        const task_content_el = document.createElement("div");
-        task_content_el.classList.add("content"); // to Z Tasks1 - content
-        task_content_el.innerText = task;
-
-        task_el.appendChild(task_content_el);
-
-        const task_input_el = document.createElement("input"); // input to typ html o typie text
-        task_input_el.classList.add("text");
-        task_input_el.type = "text";
-        task_input_el.value = task;
-        task_input_el.setAttribute("readonly", "readonly");
-
-        task_content_el.appendChild(task_input_el); // appendchild po prostu dodaje element na koniec istniejacej w html listy ...
-
-        list_el.appendChild(task_el);
-
-
-
-
-    })     
-})
- 
-const item =  //  "Create/{floor}") 
+﻿window.addEventListener('load', () =>
 {
-    floors: '',
-    walls: '',
-    roof: ''
-}
+	const form = document.querySelector("#new-task-form");
+	const input = document.querySelector("#new-task-input");
+	const list_el = document.querySelector("#tasks");
 
-const numbers =  //  "Create/{floor}") 
-{
-    number1: '',
-    operator: '',
-    number2: ''
-}
- 
+	form.addEventListener('submit', (e) =>
+	{
+		e.preventDefault();
 
-function addFloor()
-{
- //   const addItemTextbox = document.getElementById('add-floor'); // nvm co
-    const addNameTextbox = document.getElementById('add-floor');
+		const task = input.value;
 
-    item.floors = addNameTextbox.value.trim();
-    
-}
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
 
-function addWall()
-{
-    const addItemTextbox = document.getElementById('add-wall'); // nvm co
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
 
-    item.walls = addItemTextbox.value.trim();
-    
-}
-function addRoof() {
-    const addItemTextbox = document.getElementById('add-roof'); // nvm co
+		task_el.appendChild(task_content_el);
 
-    item.roof = addItemTextbox.value.trim();
+		const task_input_el = document.createElement('input');
+		task_input_el.classList.add('text');
+		task_input_el.type = 'text';
+		task_input_el.value = task;
+		task_input_el.setAttribute('readonly', 'readonly');
 
-}
+		task_content_el.appendChild(task_input_el);
 
-function myFunction()
-{
-    addFloor();
-    addWall();
-    addRoof();
-}
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
 
-function myFunction2() {
-    var floor = item.floors;
-    var wall = item.walls;
-    var roof = item.roof;
-    document.getElementById("myText").innerHTML = floor;
-    document.getElementById("myText2").innerHTML = wall;
-    document.getElementById("myText3").innerHTML = roof;
-}
+		const task_edit_el = document.createElement('button');
+		task_edit_el.classList.add('edit');
+		task_edit_el.innerText = 'Edit';
 
-function buildHouse() { 
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
 
-    fetch('api/BuildHouse', // https://localhost:7066/api/buildhouse/create/floor
-        {
-            method: 'POST',
-            headers:
-            {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(item)
-        })
-        .then(response => Response.json())
-        .then(() => {
-            //  getItems();
-           
-        })
-        .catch(error => console.error('Unable to add item.', error));
+		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_delete_el);
 
-}
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+
+		task_edit_el.addEventListener('click', (e) =>
+		{
+			if (task_edit_el.innerText.toLowerCase() == "edit")
+			{
+				task_edit_el.innerText = "Save";
+				task_input_el.removeAttribute("readonly");
+				task_input_el.focus();
+			} else
+			{
+				task_edit_el.innerText = "Edit";
+				task_input_el.setAttribute("readonly", "readonly");
+			}
+		});
+
+		task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
+	});
+});
