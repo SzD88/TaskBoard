d77db86c93d86b4ea9f86b2eeccee010a269b400
S.D <szopakleks@gmail.com>
2022-07-15 14:37:02 +0200

    cos pogrzeb w dogo

--- WebApi/wwwroot/js/Main3.js
+++ WebApi/wwwroot/js/Main3.js
@@ -1,7 +1,7 @@
 ﻿let dog_list = [];
 
 const container = document.getElementById("container");
-fetch('https://dog.ceo/api/breeds/list/all')
+fetch('api/SubTask')
     .then(response => {
         if (response.ok) {
             return response.json();
@@ -10,7 +10,8 @@ fetch('https://dog.ceo/api/breeds/list/all')
         } //   <ul id="container"></ul>
     })
     .then(data => {
-        dog_list = data.message;
+        dog_list = data[1].id;
+        console.log(data[1].id)
         for (dog in dog_list) {
             let li = document.createElement("li");
             let node = document.createTextNode(dog);
@@ -18,3 +19,25 @@ fetch('https://dog.ceo/api/breeds/list/all')
             container.appendChild(li);
         }
     });
+
+
+//let dog_list = [];
+
+//const container = document.getElementById("container");
+//fetch('https://dog.ceo/api/breeds/list/all')
+//    .then(response => {
+//        if (response.ok) {
+//            return response.json();
+//        } else {
+//            throw new Error(response.statusText);
+//        } //   <ul id="container"></ul>
+//    })
+//    .then(data => {
+//        dog_list = data.message;
+//        for (dog in dog_list) {
+//            let li = document.createElement("li");
+//            let node = document.createTextNode(dog);
+//            li.appendChild(node);
+//            container.appendChild(li);
+//        }
+//    });
